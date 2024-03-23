c4b5bd1b41ab25328f0d3749c63dfff326dff753
rhitam1 <rhitam@wintwealth.com>
2024-03-23 20:41:08 +0530

    feat: useAuth hook

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,20 @@
+// hooks/useAuth.js
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+export const useAuth = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const authToken = localStorage.getItem('authToken');
+    const isAuth = !!authToken;
+    setIsAuthenticated(isAuth);
+
+    if (!isAuth) {
+      router.push('/login');
+    }
+  }, [router]);
+
+  return { isAuthenticated };
+};

5bce86006cb4b9910df411bfa50e8298636a6092
rhitam1 <rhitam@wintwealth.com>
2024-03-23 21:13:52 +0530

    fix(categories page): Prevent display of previous user's categories before loading new data

--- src/pages/categories.tsx
+++ src/pages/categories.tsx
@@ -1,29 +1,35 @@
 // pages/categories.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HeaderComponent from '~/components/Header';
 import CategoriesComponent from '../components/Categories';
 import { useAuth } from '../hooks/useAuth';
 import { useRouter } from 'next/router';
 import { trpc } from '~/utils/trpc';
+import Loader from '~/components/Loader';
 
 const CategoriesPage = () => {
   const { isAuthenticated } = useAuth();
   const router = useRouter();
-
+  const [loading, setLoading] = useState(false);
   const userName = typeof window !== "undefined" ? localStorage.getItem('userName') || 'User' : '';
   const authToken = typeof window !== "undefined" ? localStorage.getItem('authToken') || 'User' : '';
 
   const logoutMutation = trpc.user.logout.useMutation({
+    onMutate: () => setLoading(true), 
     onSuccess: () => {
       localStorage.removeItem('authToken');
       localStorage.removeItem('userName');
+      setLoading(false); 
       router.push('/login');
     },
     onError: (error) => {
       console.error('Logout failed', error);
+      setLoading(false); 
     },
   });
 
+  if (loading) return <Loader />;
+  
   const handleLogout = () => {
     if (authToken) {
       logoutMutation.mutate({ authToken });
